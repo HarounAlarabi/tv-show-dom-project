d9fc8507d10f30294c474b3f4f4a088d797d90c3
Haroun Alarabi <alarabiharoun@gmail.com>
2023-05-01 10:54:27 +0100

    level 100 done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +6,33 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let counterEl = document.createElement('p');
+  counterEl.className = 'counter';
+  // counterEl.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.appendChild(counterEl)
+  episodeList.forEach((episode) => {
+    const episodeElem = document.createElement("div");
+    episodeElem.className = "episode";
+    let episodeName = document.createElement("h3");
+    episodeName.textContent = `${episode.name} - E0${episode.season}S0${episode.number}`;
+    let episodeImage = document.createElement("img");
+    let episodeUrl = document.createElement("a")
+    episodeUrl.href = `${episode.url}`;
+    episodeImage.src = `${episode.image.original}`;
+    let episodeDesc = document.createElement("p");
+    episodeDesc.innerHTML = `${episode.summary}`;
+
+    rootElem.appendChild(episodeElem);
+    episodeElem.appendChild(episodeName);
+    episodeElem.appendChild(episodeUrl);
+    episodeUrl.appendChild(episodeImage);
+    episodeElem.appendChild(episodeDesc);
+  });
+
 }
 
+
+
+
 window.onload = setup;
+
